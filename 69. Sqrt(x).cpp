e93e518e18572823830eb0a66d9faa40f335a394
江鄭權 <53126563+dolaenom@users.noreply.github.com>
2019-12-13 14:23:08 +0800

    Update 69. Sqrt(x).cpp

--- 69. Sqrt(x).cpp
+++ 69. Sqrt(x).cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int mySqrt(int x) {
         
-        long int i;
+        long int i; // 這裡使用long也就表示還是有個上限值，雖然比int還大。
         
         for(i=0;i*i<x;i++){
         }
