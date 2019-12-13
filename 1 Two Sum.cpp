09323e1ab36bec56084f1363919a3d13684a6c56
江鄭權 <53126563+dolaenom@users.noreply.github.com>
2019-12-13 14:21:10 +0800

    Create 1 Two Sum.cpp

---
+++ 1 Two Sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) { //需會使用Vector才容易寫。
+        
+        int first_num,sec_num;
+        vector<int> returnNums;
+        
+        for(first_num=0;first_num<nums.size();first_num++)
+        {
+            for(sec_num=first_num+1;sec_num<nums.size();sec_num++)
+            {
+                if((nums[first_num]+nums[sec_num])==target){
+                    printf("%d,%d",first_num,sec_num);
+                    returnNums.push_back(first_num);
+                    returnNums.push_back(sec_num);
+                    return returnNums;
+                }
+                   
+            }
+        }
+        
+        
+        
+        return {};
+    }
+};
