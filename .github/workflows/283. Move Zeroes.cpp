ef2ae1bfe1649cf8cb103a4f523ddb2574a84d5b
江鄭權 <53126563+dolaenom@users.noreply.github.com>
2019-12-13 14:06:38 +0800

    Create 283. Move Zeroes.cpp
    easy

---
+++ .github/workflows/283. Move Zeroes.cpp
@@ -0,0 +1,23 @@
+//額外設定了一個很醜的中斷點，要不然持續找零替換那邊會卡住。
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int counter;
+        int len = nums.size();
+        int temp = 0;
+        
+        for(counter=0;counter<len;counter++)
+        {
+            if(nums[counter]==0)
+            {
+                nums.erase(nums.begin()+counter);
+                nums.push_back(0);
+                counter--;
+            }
+            
+            temp++;
+            if(temp>2*(nums.size()))
+                break;
+        }
+    }
+};
